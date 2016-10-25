0e46d0d068db9a54e9a3b0c90939cd7bc1d511bd
mrsteele <matt@omnionline.us>
2016-10-25 15:51:38 -0400

    chore: Adding files to test for coverage

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,8 +4,19 @@
 import chai from 'chai'
 const expect = chai.expect
 
+import ContentfauxSrc from '../src'
+import ContentfauxDist from '../dist'
+
 describe('Test', () => {
   it('Should run', () => {
     expect(true).to.be.true
+  })
+
+  it('Should exist', () => {
+    expect(ContentfauxSrc).to.exist
+  })
+
+  it('Should have built', () => {
+    expect(ContentfauxDist).to.exist
   })
 })
