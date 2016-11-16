c2495ce5b7eecd9460c7fa9cde56f7058c39958a
mrsteele <matt@omnionline.us>
2016-11-16 12:02:44 -0500

    chore: Adding more coverage

--- test/index.spec.js
+++ test/index.spec.js
@@ -21,6 +21,15 @@ function test (obj, name) {
       })
     })
 
+    describe('_parseRequest', () => {
+      it('Should be able to parse query parameters properly', () => {
+        expect(obj._parseRequest('http://test.com/whatever.html?test=hi&what=ever')).to.deep.equal({
+          test: 'hi',
+          what: 'ever'
+        })
+      })
+    })
+
     describe('stub and unstub', () => {
       it('Should allow unstubbing', () => {
         expect(obj._stubbed).to.be.false
@@ -39,6 +48,7 @@ function test (obj, name) {
         obj._writeFile(`${testPath}/test.md`, 'hi')
         expect(fs.existsSync(`${testPath}/test.md`)).to.be.true
         expect(fs.readFileSync(`${testPath}/test.md`).toString()).to.equal('hi')
+        expect(obj._getContentType('Array')).to.exist
         obj._prepareFolder(testPath)
         expect(fs.existsSync(`${testPath}/test`)).to.be.false
         obj._deleteFolderRecursive(testPath)
